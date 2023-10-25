ce6dd454d30bb9bb913c4fccd37de41703452237
Isabelle Puwo <126019713+IsabellePuwo@users.noreply.github.com>
2023-10-25 15:20:35 -0400

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,29 +1,70 @@
 //Isabelle Puwo 
-// 
 //  Project 3
-//
 //  Created by Isabelle Puwo on 10/20/23.
-//
 
-#ifndef Header_h
-#define Header_h
-#include<stack>
-#include<queue>
+#ifndef ConverttoBinary_hpp
+#define ConverttoBinary_hpp
+
 #include <stdio.h>
-using namespace std;
-
-class DecimaltoBinaryConverter{
+class Stack{
 private:
-    int integer;
-    double decimal;
+    struct StackNode{
+        int data;
+        struct StackNode* next;
+    };
+    
+    StackNode *top;
 public:
-    DecimaltoBinaryConverter(double);
-    stack<int>integertoBinary();
-    queue<int> DecimaltoBinaryConversion();
-    double  decimalPlaceConverter(int);
-    void display();
+    Stack();
+    ~Stack();
+        void push(int value);
+        int Top();
+        bool isEmpty();
+        void pop();
+        int sizeOf();
+        void displayStack() const;
+       void convertionstack(int number){
+        if (number == 0) {
+                    return;
+                } else {
+                    int places = number % 2;
+                    push(places);
+                    convertionstack(number / 2);
+                }
+            }
 };
 
 
+class Queue{
+private:
+    struct QueueNode{
+        int data;
+        struct QueueNode* next;
+    };
+    QueueNode* rear;
+    QueueNode* front;
+public:
+    Queue() {
+        rear = nullptr;
+        front = nullptr;
+    }
+    ~Queue();
+    void enqueue(int data);
+    void dequeue();
+    bool isEmpty();
+    void displayQueue() const;
+    void convertionQueue(Queue &q, double decimal, int places){
+        if(places == 0) {
+            return;
+        } else {
+            decimal *= 2;
+            int digits = static_cast<int>(decimal);
+            q.enqueue(digits);
+            decimal -= digits;
+            convertionQueue(q, decimal, places - 1);
+        }
+    }
+    
+};
+#endif /* ConverttoBinary_hpp */
 
-#endif /* Header_h */
